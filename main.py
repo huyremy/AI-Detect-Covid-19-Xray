40f5e80b2ba3b61c49262960bc77aa95f3110d9e
huyremy <huynq@isi.com.vn>
2021-08-07 02:22:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import *
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-virus = load_model('covid.h5')
+virus = load_model('virus.h5')
 def output(model,img_path,size):
     img_path=img_path
     img = image.load_img(img_path,target_size=size)
