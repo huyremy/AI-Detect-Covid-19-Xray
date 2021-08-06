0ecc881d8769525d17d4d9b9e4afbfa840ae60f4
huyremy <huynq@isi.com.vn>
2021-08-06 22:38:46 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import tensorflow 
+from tensorflow.keras.layers import *
+from tensorflow.keras.preprocessing import image  
+from tensorflow.keras.models import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+virus = load_model('covid.h5')
+def output(model,img_path,size):
+    img_path=img_path
+    img = image.load_img(img_path,target_size=size)
+    img_arr = image.img_to_array(img,dtype='double')
+    img_arr=img_arr/255
+    img_arr=np.expand_dims(img_arr,axis=0)
+    result=model.predict_classes(img_arr)
+    if result==1:
+        print("Dương tính")
+    elif result==0:
+        print("Âm tính")
+img = mpimg.imread('test.jpeg')
+imgplot = plt.imshow(img)
+plt.show()
+output(virus,'test.jpeg',(224,224))
